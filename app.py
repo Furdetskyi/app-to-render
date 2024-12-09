0b1078eb5a67d31ce4922d082800b4c2a321c6a3
Furdetskyi <158449877+Furdetskyi@users.noreply.github.com>
2024-12-09 19:20:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -155,4 +155,3 @@ def delete_product(product_id):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable, default to 5000
     app.run(debug=True, host="0.0.0.0", port=port)  # Bind to 0.0.0.0 for external access
-Key Changes:
